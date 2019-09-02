8b1fea134166be8506aab1a52b70ae468cb54961
David Sadler <Davidwallacesadler@gmail.com>
2019-09-01 22:54:18 -0600

    Initial Commit

---
+++ Habit Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habit Tracker
+//
+//  Created by David Sadler on 9/1/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
